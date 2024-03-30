bcb348d4d44cbba5f612083b860c48eea0b1a160
Alex <4422504a@gmail.com>
2024-03-30 15:45:30 +0300

    -

--- appp/views.py
+++ appp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.http import (
+    HttpResponse, HttpResponseNotFound, Http404,
+    HttpResponseRedirect, HttpResponsePermanentRedirect)
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
